77ab4877bf2cfe21a6e84e49db8359bf3e31beae
S-al1 <salmaaitabbou972@gmail.comi>
2024-12-26 09:13:00 +0100

    fix une bug

---
+++ classes/Admin.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once("User.php");
+    class Admin extends User{
+        public function banUser($id_client){
+
+        }
+        public function showRes(){
+
+        }
+        public function showAct(){
+            
+        }
+    }
+
+?>
---
+++ classes/Client.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once("User.php");
+    class Client extends User{
+        public function addReservation($id_activity){
+
+        }
+        public function cancelReservation($id_res){
+
+        }
+        public function viewOffers(){
+            
+        }
+    }
+
+?>
